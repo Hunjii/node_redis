01cb740d122ac11e50f892a3adb10e2cf6102843
Hunji <Hungnmnm@gmail.com>
2020-09-08 20:21:44 +0700

    done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const redis = require("redis");
-const mongoose = require("mongoose");
-const Product = require("./Models/Product");
+const express = require('express');
+const dotenv = require('dotenv');
+const redis = require('redis');
+const mongoose = require('mongoose');
+const Product = require('./Models/Product');
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASS);
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASS);
 
 mongoose
   .connect(DB, {
@@ -15,7 +15,7 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("connect to database success!"));
+  .then(() => console.log('connect to database success!'));
 
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.PORT || 6379;
@@ -27,29 +27,36 @@ const app = express();
 const getProductDetail = async (res, req, next) => {
   try {
     const { id } = req.params.id;
-
+    // lấy sp trong csdl
     const product = await Product.findById(id);
     // check request 5 lần trong 2p
     client.get(id, (err, data) => {
-      // nếu req đén id là lần đầu tiên
+      // nếu request đến id là lần đầu tiên
       if (data === null) {
-        client.setex(id, 120, "underfined");
+        // set giá trị tạm thời cho id vào redis trong 120s
+        client.setex(id, 120, 'underfined');
         const time = Date.now();
+        // set time đầu tiên request với id trong 120s
         client.setex(`${id}:time`, 120, time);
+        // set request là 1 lần trong 120s
         client.setex(`${id}:count`, 120, 1);
       }
       // nếu req đến id ko phải lần đầu
-      else if (data === "underfined") {
-        client.get(`${id}:count`, (err, data_1) => {
-          // nếu req đến id là lần thứ 5 thì lưu product vào redis
-          if (data_1 === "5") {
+      else if (data === 'underfined') {
+        // lấy giá trị count
+        client.get(`${id}:count`, (err, data) => {
+          // nếu request đến id là lần thứ 5 thì lưu product vào redis trong 60p
+          if (data === '5') {
             client.setex(id, 3600, JSON.stringify(product));
-          } else {
-            client.get(`${id}:time`, (err, data_2) => {
+          }
+          // nếu request đến id khác lần thứ 5
+          else {
+            // lấy giá trị thời gian
+            client.get(`${id}:time`, (err, data) => {
               const time = Date.now();
-              const exprired = Math.round((time - data_2) / 1000);
-              client.setex(`${id}:time`, exprired, time);
-              client.setex(`${id}:count`, exprired, data_1 * 1 + 1);
+              // set số request + 1
+              // tính thời gian hết hạn trong redis = 120s - (time hiện tại - time lần đầu request)
+              client.setex(`${id}:count`, 120 - (time - data), data * 1 + 1);
             });
           }
         });
@@ -58,7 +65,7 @@ const getProductDetail = async (res, req, next) => {
 
     // gửi response tới client
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: product,
     });
   } catch (err) {
@@ -73,10 +80,10 @@ const cache = (req, res, next) => {
   const { id } = req.params;
 
   client.get(id, (err, data) => {
-    if (data !== null && data !== "underfined") {
+    if (data !== null && data !== 'underfined') {
       // gửi response tới client
       res.status(200).json({
-        status: "success",
+        status: 'success',
         data,
       });
     } else {
@@ -85,7 +92,8 @@ const cache = (req, res, next) => {
   });
 };
 
-app.get("/api/:id", cache, getProductDetail);
+// api lấy thông tin sp
+app.get('/api/:id', cache, getProductDetail);
 
 app.listen(5000, () => {
   console.log(`App listening in port ${PORT}`);
